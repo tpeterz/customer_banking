9f8a106fdfecd79379706d451e392e2baec1340b
tpeterz <tpetersonmich@gmail.com>
2023-11-17 00:04:19 -0500

    Finally updated and finished Mod3 Assignment

--- M3_Starter_Code/customer_banking.py
+++ M3_Starter_Code/customer_banking.py
@@ -1,29 +1,43 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
-
+from cd_account import create_cd_account
+from savings_account import create_savings_account
 # Define the main function
 def main():
     """This function prompts the user to enter the savings and cd account balance, interest rate,
-    and the length of months to determine the interest gained.
+    and the length of months to determine the interest gained.s
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
+    savings_balance = float(input("Please enter/set your savings balance: "))
+    savings_interest = float(input("Please enter/set your savings interest rate: "))
+    savings_months = int(input("Please enter/set how many months for your savings account: "))
 
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
+    updated_savings = create_savings_account(savings_balance, savings_interest, savings_months)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"Details about your updated savings account for {savings_months} months: Your Interest Earned: {updated_savings[1]}, Your Updated Balance: {updated_savings[0]}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    # Setting the CD balance, then interest_rate, then the number of months 
+    cd_balance = float(input("Please enter the CD account balance: "))
+    cd_interest_rate = float(input("Please enter the interest rate for your CD account:"))
+    cd_months = int(input("Please enter the number of months for the CD acount: "))
+   
+
 
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    updated_cd = create_cd_account(cd_balance, cd_interest_rate, cd_months)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"Details about your updated CD account for {cd_months} months: Your Interest Earned: {updated_cd[1]}, Your Updated Balance: {updated_cd[0]}")
 
 if __name__ == "__main__":
     # Call the main function.
+    main()
+    
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # customer_banking
+
+<!-- I believe that I gathered some inspiration from class activities and viewing our office hours during class. 
+
+I hope this is the correct way to complete this assignment, I am very new to banking and savings accounts(cds as well)
+
+As I was submitting this I realized that I may have been working in two different folders because I see M3_starter_Code twice
+
+It is hard to tell if there are differences, though if one does not work, the other should.
+
+It may include my thought process throughout this assignment, it may also not make sense if it is the folder with the one that does not work. -->
